c56cf1c4bdd2630008c591a64be79ce5035779e4
Paulo-Junio <paulofernandesbraga@gmail.com>
2022-04-18 12:20:10 -0300

    chat

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,22 +5,19 @@ let numeroDeMensagens=0;
 let cont=1;
 
 function entrarNaSala () {
-    console.log("entrarNaSala ()");
     usuario= prompt ("Qual o seu nome?");
     let loginUsuario = {
         name:usuario
     }
     let respostaLogin= axios.post("https://mock-api.driven.com.br/api/v6/uol/participants ",loginUsuario)
-    console.log(loginUsuario)
     respostaLogin.then(manterConexao)
     respostaLogin.catch(atualizarPagina)
     idInterval = setInterval(manterConexao, 5000)
-    setInterval(carregarMensagem, 3000)
+    let idItervalUpload = setInterval(carregarMensagem, 3000)
     
 }
 
 function manterConexao() {
-    console.log("manterConexao()");
     let statusConexao = {
         name: usuario
     };
@@ -31,7 +28,9 @@ function manterConexao() {
     }
 }
 
-function atualizarPagina (respose){
+function atualizarPagina (){
+    clearInterval(idInterval)
+    clearInterval(idItervalUpload)
     window.location.reload()
 }
 
@@ -42,31 +41,24 @@ function carregarMensagem() {
 
 function imprimirMensagens(resposta) {
     let quantidadeMensagens = resposta.data.length;
-    console.log(quantidadeMensagens)
-    console.log("numero")
-    console.log(numeroDeMensagens)
     let mensagensRecuperadas = resposta.data;
-    for (let i=(quantidadeMensagens-20); i<quantidadeMensagens; i++){
-        console.log("renderizarMensagens(for)");
+    let chat= document.querySelector(".mensagens");
+    chat.innerHTML = ""
+    for (let i=(quantidadeMensagens-50); i<quantidadeMensagens; i++){
         const privacidadeMensagem =mensagensRecuperadas[i].type;
-        let chat= document.querySelector(".mensagens");
-        chat.scrollIntoView();
         if (privacidadeMensagem === "status") {
-            console.log("renderizarMensagens(if)");
             chat.innerHTML += `<div class="box-mensagem status-chat">
             <span>(${mensagensRecuperadas[i].time})</span>
             <span><strong>${mensagensRecuperadas[i].from}</strong> ${mensagensRecuperadas[i].text}</span>
             </div>`
         }
         else if (privacidadeMensagem === "message") {
-            console.log("renderizarMensagens(else if 1)");
             chat.innerHTML += `<div class="box-mensagem mensagem-todos">
             <span>(${mensagensRecuperadas[i].time})</span>
             <span><strong>${mensagensRecuperadas[i].from}</strong> para <strong>${mensagensRecuperadas[i].to}</strong>: ${mensagensRecuperadas[i].text}</span>
             </div>`
         }
         else if (privacidadeMensagem === "private_message" && ((mensagensRecuperadas[i].to)===usuario)) {
-            console.log("renderizarMensagens(else if 2)");
             chat.innerHTML += `<div class="box-mensagem mensagem-privada">
             <span>(${mensagensRecuperadas[i].time})</span>
             <span><strong>${mensagensRecuperadas[i].from}</strong> reservadamente para <strong>${mensagensRecuperadas[i].to}</strong>: ${mensagensRecuperadas[i].text}</span>
@@ -80,7 +72,6 @@ function imprimirMensagens(resposta) {
 
 function enviarMengagem() {
     let mensagem =document.querySelector(".enviar-mensagem input").value;
-    console.log(mensagem)
     let criarMensagem = {
         from: usuario,
         to: "Todos",
